dcef3d83ff4d799dac92f37d32da4d35af524963
Robert Ricci <rmricci121@gmail.com>
2021-06-05 06:04:53 -0400

    This committ added an about and gallery template

---
+++ page-templates/gallery.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Template Name: Gallery
+ *
+ * Template for displaying a page without sidebar even if a sidebar widget is published.
+ *
+ * @package UnderStrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+
+
+
+	get_template_part( 'global-templates/hero' );
+
+?>
+
+<div class="wrapper">
+
+	<div class="container" id="content">
+
+		<div class="row">
+
+			<div class="col-md-12 content-area" id="primary">
+				<h1><?php the_field('home_title') ?></h1>
+			</div>
+		</div>
+			<div class="row home-image">
+				<img src="<?php the_field('home_image') ?>" />
+			</div>	
+
+			<div class="row">
+				<p><?php the_field('home_copy') ?></p>
+			</div><!-- #primary -->
+
+		</div><!-- .row end -->
+
+	</div><!-- #content -->
+
+
+
+<?php
+get_footer();
