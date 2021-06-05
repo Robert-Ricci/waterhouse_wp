e1f1af034fdb148a87075fc6421ba66648d1d7eb
Robert Ricci <rmricci121@gmail.com>
2021-06-05 06:24:13 -0400

    This commit added images to the gallery page

--- page-templates/gallery.php
+++ page-templates/gallery.php
@@ -18,23 +18,27 @@ get_header();
 
 ?>
 
-<div class="wrapper">
+<div >
 
-	<div class="container" id="content">
+	<div class="container">
 
 		<div class="row">
-
-			<div class="col-md-12 content-area" id="primary">
-				<h1><?php the_field('home_title') ?></h1>
+			<div class="col-md">
+			<div class="gallery-images">
+				<img src="<?php the_field('gallery_images_1') ?>" />
+				<img src="<?php the_field('gallery_images_3') ?>" />
+				<img src="<?php the_field('gallery_images_5') ?>" />
 			</div>
+			</div>
+			<div class="col-md">
+			<div class="gallery-images">
+				<img src="<?php the_field('gallery_images_2') ?>" />
+				<img src="<?php the_field('gallery_images_4') ?>" />
+				</div>
+			</div>
+			
 		</div>
-			<div class="row home-image">
-				<img src="<?php the_field('home_image') ?>" />
-			</div>	
-
-			<div class="row">
-				<p><?php the_field('home_copy') ?></p>
-			</div><!-- #primary -->
+			
 
 		</div><!-- .row end -->
 
